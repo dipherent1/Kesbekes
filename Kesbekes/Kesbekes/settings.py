17cb55aea7e8dace2a5c03bfaf0b78557291a029
dipherent1 <Bini.Blue.1996@gmail.com>
2024-07-20 02:33:08 +0300

    apps installed and settings restored

--- Kesbekes/Kesbekes/settings.py
+++ Kesbekes/Kesbekes/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'calendar_app',
+    'chatbot',
+    'users',
 ]
 
 MIDDLEWARE = [
